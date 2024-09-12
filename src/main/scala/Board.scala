a9898488cc9754fe750131c22c5a4f26c65f1501
Luka Sotra <lukasotra@gmail.com>
2024-09-12 19:58:58 +0200

    feat: add initial `board` and `cell` classes for storing and storing the game state

---
+++ src/main/scala/Board.scala
@@ -0,0 +1,84 @@
+import scala.util.Random
+
+class Board(rows: Int, cols: Int, mines: Int) {
+  private val board = Array.fill(rows, cols)(new Cell)
+
+  def getCell(row: Int, col: Int): Cell = board(row)(col)
+
+  def getRows: Int = rows
+
+  def getCols: Int = cols
+
+  def getMines: Int = mines
+
+  def getBoard: Array[Array[Cell]] = board
+
+  def loadLevel(fileContent: String): Unit = {
+//    for {
+//      row <- 0 until rows
+//      col <- 0 until cols
+//    } {
+//      board(row)(col) = new Cell
+//    }
+  }
+
+  def getCellNeighbors(row: Int, col: Int): List[Cell] = {
+    val neighbors = for {
+      i <- -1 to 1
+      j <- -1 to 1
+      if i != 0 || j != 0
+      if row + i >= 0 && row + i < rows
+      if col + j >= 0 && col + j < cols
+    } yield board(row + i)(col + j)
+
+    neighbors.toList
+  }
+
+  def placeMines(row: Int, col: Int): Unit = {
+    val random = new Random
+    var minesPlaced = 0
+
+    while (minesPlaced < mines) {
+      val randomRow = random.nextInt(rows)
+      val randomCol = random.nextInt(cols)
+
+      if (board(randomRow)(randomCol).isMine || (randomRow == row && randomCol == col)) {
+      } else {
+        board(randomRow)(randomCol).isMine = true
+        minesPlaced += 1
+      }
+    }
+  }
+
+  def calculateNeighborMines(): Unit = {
+    for {
+      row <- 0 until rows
+      col <- 0 until cols
+    } {
+      val cell = board(row)(col)
+      val neighbors = getCellNeighbors(row, col)
+      val neighborMines = neighbors.count(_.isMine)
+      cell.neighborMines = neighborMines
+    }
+  }
+
+  def revealMines(): Unit = {
+    for {
+      row <- 0 until rows
+      col <- 0 until cols
+    } {
+      val cell = board(row)(col)
+      if (cell.isMine) {
+        cell.isRevealed = true
+      }
+    }
+  }
+
+  def revealNeighbors(row: Int, col: Int): Unit = {
+    val cell = board(row)(col)
+    if (!cell.isRevealed) {
+      cell.isRevealed = true
+    }
+
+  }
+}
---
+++ src/main/scala/Cell.scala
@@ -0,0 +1,2 @@
+class Cell(var isMine: Boolean = false, var isRevealed: Boolean = false, var neighborMines: Int = 0) {
+}
